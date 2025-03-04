d0a6e8188ee85e41e914cd34b8b4618f24da71db
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-03-04 19:21:41 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,28 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static boolean validIndex(int[] arr, int index) {
+		try {
+			arr[index] = arr[index];
+		} catch (IndexOutOfBoundsException e) {
+			return false;
+		}
+		return true;
+	}
 
+	public static int divide(int a, int b) {
+		try {
+			return a / b;
+		} catch (ArithmeticException e) {
+			return 0;
+		}
+	}
+
+	public static int safeConvertStringtoInt(String convert) {
+		try {
+			return Integer.parseInt(convert);
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	}
 }
